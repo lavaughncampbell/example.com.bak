3d84b6200c79d3ada62729294b524e74e016d67b
lavaughn <lavaughnccampbell@gmail.com>
2017-12-15 10:11:54 -0600

    added new updated from personal web page

--- public/contact.php
+++ public/contact.php
@@ -6,11 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
+      <h1>Contact Lavaughn</h1>
       <nav>
         <a href="/">Home</a> |
         <a href="resume.html">Resume</a> |
         <a href="contact.php">Contact</a>
       </nav>
-    <h1>Contact</h1>
+
+      <form method="post">
+        <div>
+        <label for="firstName">First Name</label><br>
+        <input type="text" name="first_name" id="firstName">
+      </div>
+
+      <div>
+        <label for="lastName" id="lastName">Last Name</label><br>
+        <input type="text" name="last_name">
+      </div>
+
+      <div>
+        <label for="email" id="email">Email</label><br>
+        <input type="text" name="email">
+      </div>
+
+      <div>
+        <label for="subject" id="subject">Subject</label><br>
+        <input type="text" name="subject">
+      </div>
+
+      <div>
+        <label for="message" id="message">Message</label><br>
+        <textarea name="message"></textarea>
+      </div>
+
+      <input type="submit">
+    </form>
   </body>
 </html>
